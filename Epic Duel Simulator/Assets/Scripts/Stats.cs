81f4bce2a44d352c09da634421f33784b7779619
jean0314 <jean0314@live.ca>
2019-03-05 14:50:05 -0500

    Test Stats script

---
+++ Epic Duel Simulator/Assets/Scripts/Stats.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public int bladderCapacity;
+    public int strenk;
+    public int brain;
+
+    public Stats(int bladderCapacity, int strenk, int brain)
+    {
+        this.bladderCapacity = bladderCapacity;
+        this.strenk = strenk;
+        this.brain = brain;
+    }
+}
